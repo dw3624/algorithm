329852b6f01ed7e781db80b963093df98e35a367
dongwonKang <dw3624@gmail.com>
2022-07-21 14:23:16 +0900

    with answer

--- 백준/6064_카잉달력/s1.py
+++ 백준/6064_카잉달력/s1.py
@@ -2,14 +2,18 @@ import sys
 sys.stdin = open('input.txt')
 
 
+def cal(m,n,x,y):
+    k = x
+    while k <= m*n:
+        if (k-x) % m == 0 and (k-y) % n == 0:
+            return k
+        k += m
+    return -1
+
+
 T = int(input())
 for _ in range(T):
-    M, N, x, y = map(int, input().split())
+    M, N, X, Y = map(int, input().split())
 
-    while x < M * N:
-        if not (x-y) % N:
-            print(x)
-            break
-        x += M
-    else:
-        print(-1)
+    res = cal(M,N,X,Y)
+    print(res)
