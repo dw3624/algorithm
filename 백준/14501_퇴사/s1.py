b779d15439ff0fe6b5af8a99b484211bae27566e
dongwonKang <dw3624@gmail.com>
2022-07-26 19:36:05 +0900

    with answer

--- 백준/14501_퇴사/s1.py
+++ 백준/14501_퇴사/s1.py
@@ -13,10 +13,11 @@ for i in range(N-1, -1, -1):
     if T[i] + i > N:
         dp[i] = dp[i+1]
     else:
+        # i+1번째 날 기준 수익, i번째 날 수익 + Ti만큼 지난 후 수익 비교
         dp[i] = max(dp[i+1], P[i]+dp[i+T[i]])
 
-    # print('T:', T)
-    # print('P:', P)
-    # print('dp:', dp)
-    # print()
+    print('T:', T)
+    print('P:', P)
+    print('dp:', dp)
+    print()
 print(dp[0])
---
+++ 백준/1699_제곱수의합/s1.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+N = int(input())
+dp = [x for x in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, i):
+        if i < j**2:
+            break
+        if dp[i-j**2]+1 < dp[i]:
+            dp[i] = dp[i-j**2]+1
+print(dp[-1])
