ca11d0632d7189e37dcc199250ef46faf532ccfb
dongwonKang <dw3624@gmail.com>
2022-08-22 21:16:46 +0900

    with hint

--- 백준/15654_N과M(5)/s1.py
+++ 백준/15654_N과M(5)/s1.py
@@ -4,5 +4,14 @@ sys.stdin = open('input.txt')
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
 nums.sort()
-print(nums)
 
+def back(lst, cnt):
+    if cnt == M:
+        print(*lst)
+    for num in nums:
+        if num not in lst:
+            lst.append(num)
+            back(lst, cnt+1)
+            lst.pop()
+
+back([], 0)
