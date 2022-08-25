108b845a83254cdc10bdde5cecfede38c80adfd1
dongwonKang <dw3624@gmail.com>
2022-08-25 22:01:53 +0900

    with hint

--- 백준/11725_트리의부모찾기/s1.py
+++ 백준/11725_트리의부모찾기/s1.py
@@ -1,27 +1,29 @@
 import sys
 sys.stdin = open('input.txt')
 
+
+from collections import deque
+import sys
+
 N = int(input())
-
-# 1-6-3-5
-#  ㄴ4-2
-#    ㄴ7
-
-# 1 6 4
-# 2 4
-# 3 6 5
-# 4 1 2 7
-# 5 3
-# 6 1 3
-# 7 4
-node_set = [list(map(int, input().split())) for _ in range(N-1)]
-node_lst = [[] for _ in range(N+1)]
-
-for n in node_set:
-    node_lst[n[0]].append(n[1])
-    node_lst[n[1]].append(n[0])
-print(node_lst)
-
+node_set = [list(map(int, sys.stdin.readline().split())) for _ in range(N-1)]
 node_dict = {}
+for n in node_set:
+    node_dict.setdefault(n[0], []).append(n[1])
+    node_dict.setdefault(n[1], []).append(n[0])
+
+q = deque([1])
+nodes = [0]*(N+1)
+while q:
+    v = q.popleft()
+    for child in node_dict[v]:
+        if nodes[v] != child:
+            nodes[child] = v
+            q.append(child)
+print(*nodes[2:], sep='\n')
+
+
+
+
 
 
