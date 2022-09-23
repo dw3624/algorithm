05e12e3ebd9182de7ce74da60d3099dc10858cb0
Dongwon Kang <dw3624@gmail.com>
2022-09-23 15:12:52 +0900

    with answer

--- 백준/11404_플로이드/s1.py
+++ 백준/11404_플로이드/s1.py
@@ -4,25 +4,24 @@ sys.stdin = open("input.txt")
 
 n = int(input())
 m = int(input())
-costs = [[100001 for _ in range(n + 1)] for _ in range(n + 1)]
-for r in range(1, n + 1):
-    for c in range(1, n + 1):
-        if r == c:
-            costs[r][c] = 0
+graph = [[int(1e9)]*(n+1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    graph[a][a] = 0
+
 for _ in range(m):
     a, b, c = map(int, input().split())
-    costs[a][b] = min(c, costs[a][b])
+    graph[a][b] = min(c, graph[a][b])
 
 for i in range(1, n + 1):
     for r in range(1, n + 1):
         for c in range(1, n + 1):
-            if r != c:
-                costs[r][c] = min(costs[r][c], costs[r][i] + costs[i][c])
+            graph[r][c] = min(graph[r][c], graph[r][i] + graph[i][c])
 
-for r in costs[1:]:
-    for c in r[1:]:
-        if c == 100001:
+for r in range(1, n+1):
+    for c in range(1, n+1):
+        if graph[r][c] == int(1e9):
             print(0, end=" ")
         else:
-            print(c, end=" ")
+            print(graph[r][c], end=" ")
     print()
